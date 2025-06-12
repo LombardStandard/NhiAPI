5698565745bc1a65a567ba6ee94a71e25e63de07
Chris Summa <csumma@uno.edu>
2025-06-12 12:40:44 +0900

    upload application

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # NhiAPI
-A simple API that handles basic HTTP requests using FastAPI and AWS EC2. 
+This simple REST API supports basic CRUD requests. It was made using FastAPI and hosted on an AWS EC2 instance.
+
+-----
+
+# Usage
+The API is accessible from the root endpoint at http://localhost:8000. 
+
+Items can also be created, read, updated, and deleted from the inventory as in the following examples:
+
+## POST
+[/items/](http://localhost:8000/items/): creates a new item. 
+Body:
+```json
+{
+  "name": "Example item",
+  "description": "An example item."
+}
+```
+
+## GET
+/items/{item_id}: read an item by its uniform resource identifier. 
+Response:
+```json
+{
+  "name": "Example item",
+  "description": "An example item."
+}
+```
+
+## PUT
+/items/{item_id}: update an item using its uniform resource identifier. 
+Body:
+```json
+{
+  "name": "Updated item",
+  "description": "Updated item description."
+}
+```
+
+## DELETE
+/items/{item_id}: delete an item using its uniform resource identifier. 
+Response:
+```json
+{
+  "name": "Deleted item",
+  "description": "Deleted item description."
+}
---
+++ main.py
@@ -0,0 +1,56 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+### GET method to display "Hello World"
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello World"}
+
+
+
+### CRUD operations
+
+# in-memory database
+items = []
+
+# define Item data model with Pydantic BaseModel
+class Item(BaseModel):
+    name: str
+    description: str
+
+# POST method to create an item
+@app.post("/items/", response_model=Item)
+async def create_item(item: Item):
+   items.append(item)
+   return item
+
+# API endpoint for GET item requests
+@app.get("/items/{item_id}", response_model=Item)
+async def read_item(item_id: int):
+    if item_id < 0 or item_id >= len(items):
+        raise HTTPException(status_code=404, detail="Item not found")
+    return items[item_id]
+
+# UPDATE method for items
+@app.put("/items/{item_id}", response_model=Item)
+async def update_item(item_id: int, item: Item):
+    if item_id < 0 or item_id >= len(items):
+        raise HTTPException(status_code=404, detail="Item not found")
+    
+    items[item_id] = item
+    return item
+
+# DELETE method 
+@app.delete("/items/{item_id}", response_model=Item)
+async def delete_item(item_id: int):
+    if item_id < 0 or item_id >= len(items):
+        raise HTTPException(status_code=404, detail="Item not found")
+    
+    deleted_item = items.pop(item_id)
+    return deleted_item
+
+# database to store items
